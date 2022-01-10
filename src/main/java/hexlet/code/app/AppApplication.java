d0fe2e3904d8a8bc32f5baaba611885d0b9169c3
MadMan <mr.mad2k@gmail.com>
2022-01-10 22:32:50 +0100

    test commit

--- src/main/java/hexlet/code/app/AppApplication.java
+++ src/main/java/hexlet/code/app/AppApplication.java
@@ -3,6 +3,7 @@ package hexlet.code.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Main class
 @SpringBootApplication
 public class AppApplication {
 

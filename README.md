0c17c11930d1df44bd7a36205db84dadcbb67a4d
MadMan <mr.mad2k@gmail.com>
2022-01-12 22:04:59 +0100

    add migration logic

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/MadMan2k/java-project-lvl5/workflows/hexlet-check/badge.svg)](https://github.com/MadMan2k/java-project-lvl5/actions)
+
+[![Java CI](https://github.com/MadMan2k/java-project-lvl5/actions/workflows/main.yml/badge.svg)](https://github.com/MadMan2k/java-project-lvl5/actions/workflows/main.yml)
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/965d839e48dfbbb352a3/maintainability)](https://codeclimate.com/github/MadMan2k/java-project-lvl5/maintainability)
+
+[![Test Coverage](https://api.codeclimate.com/v1/badges/965d839e48dfbbb352a3/test_coverage)](https://codeclimate.com/github/MadMan2k/java-project-lvl5/test_coverage)
+
+
+
+Check demo version on [Heroku](https://task3r-app.herokuapp.com/)
---
+++ src/main/java/hexlet/code/app/entity/User.java
@@ -0,0 +1,33 @@
+package hexlet.code.app.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private LocalDateTime createdAt;
+}

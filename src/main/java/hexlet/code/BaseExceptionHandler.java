5401ac6f7f7d6eaa4f9f9d6874a65bea9760b048
MadMan <mr.mad2k@gmail.com>
2022-02-13 13:20:05 +0100

    checkstyle fix

--- src/main/java/hexlet/code/BaseExceptionHandler.java
+++ src/main/java/hexlet/code/BaseExceptionHandler.java
@@ -26,48 +26,80 @@ import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 @ControllerAdvice
 public class BaseExceptionHandler {
 
+    /**
+     * @param exception
+     * @return exception message
+     */
     @ResponseStatus(INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
     public String generalExceptionHandler(Exception exception) {
         return exception.getMessage();
     }
 
+    /**
+     * @param exception
+     * @return exception message
+     */
     @ResponseStatus(NOT_FOUND)
     @ExceptionHandler(NoSuchElementException.class)
     public String noSuchElementExceptionHandler(NoSuchElementException exception) {
         return exception.getMessage();
     }
 
+    /**
+     * @param exception
+     * @return exception message
+     */
     @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public String validationExceptionsHandler(Exception exception) {
         return exception.getMessage();
     }
 
+    /**
+     * @param exception
+     * @return exception message
+     */
     @ResponseStatus(UNPROCESSABLE_ENTITY)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public List<ObjectError> validationExceptionsHandler(MethodArgumentNotValidException exception) {
         return exception.getAllErrors();
     }
 
+    /**
+     * @param exception
+     * @return exception message
+     */
     @ResponseStatus(UNPROCESSABLE_ENTITY)
     @ExceptionHandler(DataIntegrityViolationException.class)
     public String validationExceptionsHandler(DataIntegrityViolationException exception) {
         return exception.getCause().getCause().getMessage();
     }
 
+    /**
+     * @param exception
+     * @return exception message
+     */
     @ResponseStatus(FORBIDDEN)
     @ExceptionHandler(AccessDeniedException.class)
     public String accessDeniedException(AccessDeniedException exception) {
         return exception.getMessage();
     }
 
+    /**
+     * @param exception
+     * @return exception message
+     */
     @ResponseStatus(UNAUTHORIZED)
     @ExceptionHandler(UsernameNotFoundException.class)
     public String userNotFoundExceptionHandler(UsernameNotFoundException exception) {
         return exception.getMessage();
     }
 
+    /**
+     * @param exception
+     * @return exception message
+     */
     @ResponseStatus(UNPROCESSABLE_ENTITY)
     @ExceptionHandler(ConstraintViolationException.class)
     public String deleteExceptionHandler(ConstraintViolationException exception) {
